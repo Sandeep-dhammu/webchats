2b6c272ac2add135da0b779ee109cbcd5f7d6b13
sandeep.07 <sandeep.ivanfotech@gmail.com>
2023-11-29 04:02:41 +0530

    user listing Ui and api implemention

--- src/app/http-handler/generic-api.ts
+++ src/app/http-handler/generic-api.ts
@@ -4,19 +4,20 @@ import {
   HttpParams,
 } from '@angular/common/http';
 import { Observable, catchError, throwError } from 'rxjs';
+import { User } from '../model/User.model';
 
-export class GenericApi {
+export class GenericApi<TModal> {
   apiUrl: string;
   constructor(key: string, private http: HttpClient) {
-    this.apiUrl = 'http://localhost:2200/api/' + key + '/';
+    this.apiUrl = 'http://localhost:2200/api/' + key;
   }
 
-  search(query: Object): any {
+  search(query: any): any {
     return new Promise((resolve, reject) => {
-      try {
-        this.http.get(this.apiUrl)?.subscribe({
-          next: (n) => resolve(n),
-          error: (e) => this.handleError(e),
+      try {      
+        this.http.get<TModal>(this.apiUrl, {params:query})?.subscribe({
+          next: (val) => resolve(val),
+          error: (err) => this.handleError(err),
         });
       } catch (err) {
         reject(err);
@@ -27,9 +28,12 @@ export class GenericApi {
   create(url: string, body: Object): any {
     return new Promise((resolve, reject) => {
       try {
-        this.http.post(this.apiUrl + url, body)?.subscribe({
-          next: (n) => resolve(n),
-          error: (e) => this.handleError(e),
+        this.http.post<any>(`${this.apiUrl}/${url}` , body)?.subscribe({
+          next: (val) => {
+            if(val.status  == "error") reject(val?.message ??  val)
+            resolve(val)
+          },
+          error: (err) => this.handleError(err),
         });
       } catch (err) {
         reject(err);
---
+++ src/app/services/users.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { GenericApi } from '../http-handler/generic-api';
+import { HttpClient } from '@angular/common/http';
+import { User } from '../model/User.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UsersService {
+  users = new GenericApi<User>('users', this._http);
+  constructor(private _http: HttpClient) {}
+
+  fetchList(query:any){
+    try {
+      return this.users.search(query)
+    } catch (err) {
+      throw err
+    }
+  }
+}
